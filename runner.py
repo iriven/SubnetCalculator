fedf1ee0dfeeb53790d0ea8c46940dcaf1fbd8bb
Alfred Tchondjo <iriven@yahoo.fr>
2021-04-05 20:19:08 +0200

    Create runner.py

---
+++ runner.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    IpPrefix = input('Enter IP address in IP/Mask Form : ')
+    net = subnetCalculator(IpPrefix)
+    net.toString()
+#   print('Hosts List : ' , net.Hosts())
+#   print('Parent Network : ' , net.parentNetwork(prefixlenDiff=5))
+#   print('Subnets Blocks : ' , net.subNetworks(newPrefix=21))
